f2c2b824696681e934ff59d7f3c5204f13053847
Marek Hajdučák <hajducakmarek@gmail.com>
2023-09-14 15:52:00 +0200

    Initial Commit

---
+++ Livesport/LivesportApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivesportApp.swift
+//  Livesport
+//
+//  Created by MacBook Pro on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct LivesportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
